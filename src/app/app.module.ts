3a15c5a3340a9682e0ef151cb2a86530a1f41bfb
Felipe Borges <felipe.borges@xcaliber.com>
2020-09-08 00:15:10 +0200

    Home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuModule } from './menu/menu.module';
+import { HomeModule } from './home/home.module';
+import {SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
+    SharedModule,
     BrowserAnimationsModule,
     MenuModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
